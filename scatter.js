bc328fb42b450ab179a0c6e31968aaea5dd2c397
claire <amoss.claire@gmail.com>
2018-11-15 11:53:52 -0600

    added tooltips

--- scatter.js
+++ scatter.js
@@ -1,6 +1,7 @@
 d3.csv("data.csv").then(function(data) {
 	const draw_scp = (...ind_vars) => {
 		var div = document.getElementsByClassName("scatter")[0];
+		console.log(div);
 		var width = parseInt(getComputedStyle(div).width.replace(/px/g, "")),
 		height = parseInt(getComputedStyle(div).height.replace(/px/g, "")),
 		margins = getComputedStyle(div).margin.replace(/px/g, "").split(" "),
@@ -10,6 +11,10 @@ d3.csv("data.csv").then(function(data) {
 			"bottom": parseInt(margins[2]),
 			"left": parseInt(margins[3])
 		};
+
+		tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip-scp")
+        .style("opacity", 0);
 
 		y = d3.scaleLinear()
 		.domain([d3.min(data, d => (Number(d.college_p)-0.05)), d3.max(data, d => (Number(d.college_p)+0.1))]).nice()
@@ -68,7 +73,11 @@ d3.csv("data.csv").then(function(data) {
 		.attr("cx", d => x(d.vote_p))
 		.attr("cy", d => y(d.college_p))
 		.attr("fill", d => z(d.vote_p))
-		.attr("r", 2.5);
+		.attr("r", 2.5)
+		.attr("id", d => d.site_id)
+		.on("mouseover", mouseover)
+		.on("click", click)
+		.on("mouseout", mouseout);
 
 		svg.append("line")
 		.attr("x1", x(0.5))  
@@ -78,6 +87,56 @@ d3.csv("data.csv").then(function(data) {
 		.style("stroke-width", 1)
 		.style("stroke", "#D3D3D3")
 		.style("fill", "none");
+
+		function mouseover(d) {
+			var mouse = d3.mouse(this);
+			
+			d3.selectAll("#"+d.site_id)
+			.attr("r", 4)
+			.attr("stroke-opacity", 0.7);
+
+			tooltip
+			.text(function () { return d.site_id;})
+			.style("left", (d3.event.pageX + 10) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+
+			tooltip
+			.transition()
+			.duration(500)
+			.style("opacity", 1);
+		}
+
+		function click(d) {
+			var mouse = d3.mouse(this);
+			
+			d3.selectAll("#"+d.site_id)
+			.attr("r", 4)
+			.attr("stroke-opacity", 0.7);
+
+			tooltip
+			.attr("transform", "translate(" + mouse[0] + "," + mouse[1] + ")scale(0)")
+			.text(function () { return d.site_id;});
+
+			tooltip
+			.transition()
+			.duration(500)
+			.attr("x", mouse[0])
+			.attr("y", mouse[1])
+			.style("opacity", 1)
+			.attr("transform", "scale(1)");
+		}
+
+		function mouseout(d) {
+			var mouse = d3.mouse(this);
+
+			d3.selectAll("#"+d.site_id)
+			.attr("r", 2.5)
+			.attr("stroke-opacity", 0.2);
+
+            tooltip.transition()
+            .duration(500)
+            .style("opacity", 0);
+		}
 
 		return svg.node();
 	}

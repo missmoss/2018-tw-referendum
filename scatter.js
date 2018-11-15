24a26a686be33c15ba78ca295056e039ed3b9ca2
claire <amoss.claire@gmail.com>
2018-11-15 12:47:34 -0600

    flexible inputs, added dummy data

--- scatter.js
+++ scatter.js
@@ -1,7 +1,7 @@
 d3.csv("data.csv").then(function(data) {
-	const draw_scp = (...ind_vars) => {
+	const draw_scp = (vote, ...ind_vars) => {
 		var div = document.getElementsByClassName("scatter")[0];
-		console.log(div);
+
 		var width = parseInt(getComputedStyle(div).width.replace(/px/g, "")),
 		height = parseInt(getComputedStyle(div).height.replace(/px/g, "")),
 		margins = getComputedStyle(div).margin.replace(/px/g, "").split(" "),
@@ -16,22 +16,10 @@ d3.csv("data.csv").then(function(data) {
         .attr("class", "tooltip-scp")
         .style("opacity", 0);
 
-		y = d3.scaleLinear()
-		.domain([d3.min(data, d => (Number(d.college_p)-0.05)), d3.max(data, d => (Number(d.college_p)+0.1))]).nice()
-		.range([height - margin.bottom, margin.top]);
-
+        // x(vote) & z(color-mapping) are common for all plots
 		x = d3.scaleLinear()
-		.domain([d3.min(data, d => (Number(d.vote_p)-0.05)), d3.max(data, d => (Number(d.vote_p)+0.05))]).nice()
+		.domain([d3.min(data, d => (Number(d[vote])-0.05)), d3.max(data, d => (Number(d[vote])+0.05))]).nice()
 		.range([margin.left, width - margin.right]);
-
-		z = d3.scaleThreshold()
-		.range(["#355C7D", "#F67280"]);
-
-		const svg = d3.select("#scp1").append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 		// Add the x-axis.
 		xAxis = g => g
@@ -39,61 +27,87 @@ d3.csv("data.csv").then(function(data) {
 		.call(d3.axisBottom(x).ticks(width / 80).tickFormat(d3.format(".0%")))
 		.call(g => g.select(".domain").remove());
 
-		// Add the y-axis.
-		yAxis = g => g
-		.attr("transform", `translate(${margin.left},0)`)
-		.call(d3.axisLeft(y).tickFormat(d3.format(".0%")))
-		.call(g => g.select(".domain").remove())
-		.call(g => g.selectAll(".tick line")
-			.filter(d => d === 0)
-			.clone()
-			.attr("x2", width - margin.right - margin.left)
-			.attr("stroke", "#ccc"))
-		.call(g => g.append("text")
-			.attr("fill", "#000")
-			.attr("x", 5)
-			.attr("y", margin.top)
-			.attr("dy", "0.32em")
-			.attr("text-anchor", "start")
-			.attr("font-weight", "bold")
-			.text("college_percentage"))
+		z = d3.scaleThreshold()
+		.range(["#355C7D", "#F67280"]);
 
-		svg.append("g")
-		.call(xAxis);
+		ind_vars.forEach(function(ind_var, i) {
 
-		svg.append("g")
-		.call(yAxis);
+			var scpdiv;
+			if(i===0) {
+				scpdiv = d3.select("#scp0");
+			} else {
+				scpdiv = d3.select("#scatter-block")
+				.append("div")
+				.attr("id", "scp"+i)
+				.attr("class", "scatter");
+			}
 
-		svg.append("g")
-		.attr("stroke", "#000")
-		.attr("stroke-opacity", 0.2)
-		.selectAll("circle")
-		.data(data)
-		.enter().append("circle")
-		.attr("cx", d => x(d.vote_p))
-		.attr("cy", d => y(d.college_p))
-		.attr("fill", d => z(d.vote_p))
-		.attr("r", 2.5)
-		.attr("id", d => d.site_id)
-		.on("mouseover", mouseover)
-		.on("click", click)
-		.on("mouseout", mouseout);
+			const svg = scpdiv.append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-		svg.append("line")
-		.attr("x1", x(0.5))  
-		.attr("y1", 0)
-		.attr("x2", x(0.5))  
-		.attr("y2", height - margin.top - margin.bottom)
-		.style("stroke-width", 1)
-		.style("stroke", "#D3D3D3")
-		.style("fill", "none");
+			// y is customized by input variables
+			y = d3.scaleLinear()
+			//.domain([d3.min(data, d => (Number(d[ind_var])-0.02)), d3.max(data, d => (Number(d[ind_var])+0.1))]).nice()
+			.domain([0, d3.max(data, d => (Number(d[ind_var])+0.1))]).nice()
+			.range([height - margin.bottom, margin.top]);
+
+			// Add the y-axis.
+			yAxis = g => g
+			.attr("transform", `translate(${margin.left},0)`)
+			.call(d3.axisLeft(y).tickFormat(d3.format(".0%")))
+			.call(g => g.select(".domain").remove())
+			.call(g => g.selectAll(".tick line")
+				.filter(d => d === 0)
+				.clone()
+				.attr("x2", width - margin.right - margin.left)
+				.attr("stroke", "#ccc"))
+			.call(g => g.append("text")
+				.attr("fill", "#000")
+				.attr("x", 5)
+				.attr("y", margin.top)
+				.attr("dy", "0.32em")
+				.attr("text-anchor", "start")
+				.attr("font-weight", "bold")
+				.text(ind_var))
+
+			svg.append("g")
+			.call(xAxis);
+
+			svg.append("g")
+			.call(yAxis);
+
+			svg.append("g")
+			.attr("stroke", "#000")
+			.attr("stroke-opacity", 0.2)
+			.selectAll("circle")
+			.data(data)
+			.enter().append("circle")
+			.attr("cx", d => x(d[vote]))
+			.attr("cy", d => y(d[ind_var]))
+			.attr("fill", d => z(d[vote]))
+			.attr("r", 2.5)
+			.attr("id", d => d.site_id)
+			.on("mouseover", mouseover)
+			.on("click", click)
+			.on("mouseout", mouseout);
+
+			svg.append("line")
+			.attr("x1", x(0.5))  
+			.attr("y1", 0)
+			.attr("x2", x(0.5))  
+			.attr("y2", height - margin.top - margin.bottom)
+			.style("stroke-width", 1)
+			.style("stroke", "#D3D3D3")
+			.style("fill", "none");
+		}); // end of ind_vars.forEach()
 
 		function mouseover(d) {
-			var mouse = d3.mouse(this);
-			
 			d3.selectAll("#"+d.site_id)
-			.attr("r", 4)
-			.attr("stroke-opacity", 0.7);
+			.attr("r", 6)
+			.attr("fill", "#f9fe6c");
 
 			tooltip
 			.text(function () { return d.site_id;})
@@ -107,38 +121,32 @@ d3.csv("data.csv").then(function(data) {
 		}
 
 		function click(d) {
-			var mouse = d3.mouse(this);
-			
 			d3.selectAll("#"+d.site_id)
-			.attr("r", 4)
-			.attr("stroke-opacity", 0.7);
+			.attr("r", 6)
+			.attr("fill", "#f9fe6c");
 
 			tooltip
-			.attr("transform", "translate(" + mouse[0] + "," + mouse[1] + ")scale(0)")
-			.text(function () { return d.site_id;});
+			.text(function () { return d.site_id;})
+			.style("left", (d3.event.pageX + 10) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
 
 			tooltip
 			.transition()
 			.duration(500)
-			.attr("x", mouse[0])
-			.attr("y", mouse[1])
-			.style("opacity", 1)
-			.attr("transform", "scale(1)");
+			.style("opacity", 1);
 		}
 
 		function mouseout(d) {
-			var mouse = d3.mouse(this);
-
 			d3.selectAll("#"+d.site_id)
 			.attr("r", 2.5)
-			.attr("stroke-opacity", 0.2);
+			.attr("fill", d => z(d[vote]));
 
             tooltip.transition()
             .duration(500)
             .style("opacity", 0);
 		}
 
-		return svg.node();
+		//return svg.node();
 	}
-	draw_scp(); 
+	draw_scp("vote14", "college_p", "divorce_p"); 
 });
